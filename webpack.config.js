8a1c762c377c4f93e97429506d8ce18321ca9197
Matheus <matheus.marques@limberchef.com.br>
2024-01-09 17:52:23 -0300

    webpack update

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
     path: path.resolve(__dirname, 'public', 'assets', 'js'),
     filename: 'bundle.js'
   },
+  target: 'node',
   module: {
     rules: [{
       exclude: /node_modules/,
